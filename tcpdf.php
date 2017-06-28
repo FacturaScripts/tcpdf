055ae2b2eb4cba4b58726ee8af772ee2a969177c
Francesc Pineda Segarra <shawe.ewahs@gmail.com>
2017-06-28 20:19:14 +0200

    - Solucionado bug de TCPDF que causaba que en determinadas situaciones envía contenido antes de tiempo.

--- tcpdf.php
+++ tcpdf.php
@@ -18831,7 +18831,9 @@ Putting 1 is equivalent to putting 0 and calling Ln() just after. Default value:
 			}
 			case 'a': {
 				if (array_key_exists('href', $tag['attribute'])) {
-					$this->HREF['url'] = $tag['attribute']['href'];
+					if (isset($this->HREF['url'])) {
+						$this->HREF['url'] = $tag['attribute']['href'];
+					}
 				}
 				break;
 			}
